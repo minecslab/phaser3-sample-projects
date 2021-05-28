1373eb313930d5a27f884918c16e5dabbc1090a1
Leo Barrameda <leo@trezoroloyalty.com>
2021-05-28 21:37:25 +0800

    Part 5 - Ready Player One

--- src/index.js
+++ src/index.js
@@ -32,6 +32,8 @@ function preload() {
 
 var platforms;
 
+var player;
+
 function create() {
     // 0,0 is the center of the canvas - so 400,300 means upper left corner.
     this.add.image(400, 300, 'sky');
@@ -54,6 +56,27 @@ function create() {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'turn',
+        frames: [{key: 'dude', frame: 4}],
+        frameRate: 20
+    });
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update() {

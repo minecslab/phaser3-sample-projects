5e10a467f8803e935cf2320f2a6634ed8272bbba
Leo Barrameda <leo@trezoroloyalty.com>
2021-05-28 21:22:36 +0800

    add platform

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    // we need this to use this.physics - flag that game will be  using arcade physics system. 
+    // we need this to use this.physics - flag that game will be  using arcade physics system.
     physics: {
         default: 'arcade',
         arcade: {
@@ -36,9 +36,21 @@ function create() {
     // 0,0 is the center of the canvas - so 400,300 means upper left corner.
     this.add.image(400, 300, 'sky');
     // you are using arcade physics system
+    //create static physics group
     platforms = this.physics.add.staticGroup();
+    //2 types of physics body:
+    // 1. dynamic - one that can move around via forces such as velocity or accelration.
+    //              - can bounce and collide with other objects and that collision is influenced by the mass of the
+    //              body and other elements
+    // 2. static - simply has position and size.
 
+    // group - a group together similar objects and control them all as one single unit.
+    //          - can also check for collision between groups and other game objects.
+
+    // create ground
+    // set scale 2 so that it will cover the whole width.
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    // add items to group
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
